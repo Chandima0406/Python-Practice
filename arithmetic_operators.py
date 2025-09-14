d5a12f232a5cdee5bcdd774a8e79ce9d6ceb409f
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-09-14 11:58:34 +0530

    Arithmetic_operators py

---
+++ arithmetic_operators.py
@@ -0,0 +1,34 @@
+a = 11
+b = 3
+
+#addition 
+c = a+ b
+print(c)
+
+#substraction
+d = a - b
+print(d)
+
+#multiplication
+e = a*b
+print(e)
+
+#division
+f = a/b
+print(f)
+
+# floor division
+g = a//b
+print(g)
+
+# mod
+h = a%b
+print(h)
+
+# power
+i = a**b
+print(i)
+
+# operater precedence
+j = (5 % 4) * 2 + (6 // 4)
+print(j)

49b0c4f4b2dcca273cf022dae6399171e3ef4814
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-09-14 18:12:15 +0530

    logical operators py

---
+++ logical_operators.py
@@ -0,0 +1,16 @@
+print(True and True)
+print(True and False)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or False)
+
+print(not False)
+print(not True)
+
+print((5 > 6) and (5 > 9))
+print((5 < 6) and (5 < 9))
+
+print((5 > 6) or (5 > 9))
+print((5 < 6) or (5 < 9))

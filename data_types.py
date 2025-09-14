b4030b4cef8d8291039a1bc6aa372ddae1264e52
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-09-14 10:58:45 +0530

    Data types

---
+++ data_types.py
@@ -0,0 +1,29 @@
+#integer
+a = 10
+b = -25
+
+print(a)
+print(type(a))
+print(a.__sizeof__())
+
+#float
+c = 11.5
+d = -5.6
+
+print(type(c))
+
+#Bool
+e = True
+f = False
+
+print(e)
+print(type(e))
+
+# Complex
+g = 5+6j
+h = complex(10,20) # complex difine krnna puluwan thawath widiyak meka 
+
+print(g)
+print(type(g))
+print(g.real)
+print(g.imag)

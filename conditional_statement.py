f2bdc1b1e974a170ab24eab90b96f282ad849394
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-09-14 19:08:34 +0530

    Conditional Statement

---
+++ conditional_statement.py
@@ -0,0 +1,27 @@
+marks = 70
+
+if marks >= 50:
+    print("Pass")
+else:
+    print("Fail")
+
+
+
+marks1 = 30
+
+if marks1 >= 50:
+    print("Pass")
+else:
+    print("Fail")  
+
+
+marks2 = 80
+
+if marks2 > 75:
+    print("A")
+elif marks2 > 65:
+    print("B")
+elif marks2 > 50:
+    print("C")
+else:
+    print("F")

488908742eb5113ca689218f0bd9aa8ad6016b51
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-09-14 23:24:02 +0530

    Printing py

---
+++ printing.py
@@ -0,0 +1,13 @@
+print("Hello World")
+print("Hello \nWorld")
+print("Hellowwww \rWorld")
+print("Hellowwww\tWorld")
+print("Hellowwww\t\tWorld")
+
+name = "Kamal"
+age = 22
+score = 55.2
+
+print(name, end='-')
+print(age, end='-')
+print(score,end='-')

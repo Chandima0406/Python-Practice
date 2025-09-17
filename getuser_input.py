0f21d9f3750b5e6b71de9a6badbf560ed7d54cce
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-09-17 06:27:25 +0530

    getuser input py

---
+++ getuser_input.py
@@ -0,0 +1,17 @@
+name = input("Enater Your name: ")
+age = int(input("Enter your age: "))
+score = float(input("Enter your score: "))
+
+print(f"Name is {name}") 
+print(f"Age is {age}")
+print(f"Score is {score}")
+
+print(type(age))
+print(type(score))
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+
+print(f"Answer is {sum}")

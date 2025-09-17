7ddb5af3a815f86756f24db973de6861b581fb38
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-09-17 09:18:14 +0530

    stringdata_type

---
+++ stringdata_type.py
@@ -0,0 +1,16 @@
+name = "Chandima wjjerathna"
+
+print(name[0])
+print(name[1])
+print(name[0:9])
+print("C" in name)
+print("c" not in name)
+print("Chandima" not in name)
+
+
+s1 = "python"
+s2 = "Programming"
+s3 = 22
+
+print(s1+" "+s2+" "+str(s3))
+

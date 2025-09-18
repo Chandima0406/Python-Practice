e51213af95b607a76bb44766f169f2191affbc2a
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-09-18 21:13:16 +0530

    string_method2

---
+++ string_method2.py
@@ -0,0 +1,24 @@
+name = "chandima wijerathna"
+
+# startwith(), endwith()
+print(name.startswith("c"))
+print(name.startswith("h"))
+
+print(name.endswith("a"))
+print(name.endswith("n"))
+
+a = "kamal, 22, colombo"
+#replace
+print(a)
+print(a.replace(",", "-"))
+print(a.replace("l", "Z"))
+
+#join
+x = "-"
+y = "efgh"
+
+print(x.join(y))
+
+#split
+z = "Nimal Kamal Saman"
+print(z.split(" "))

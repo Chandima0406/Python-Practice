42a86ba72361d50286d6e3edc156d5fb2cee6dfa
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-09-19 16:16:25 +0530

    Tupledata_type

---
+++ Tupledata_type.py
@@ -0,0 +1,18 @@
+tpl1 = (22,25,56,89)
+tpl2 = tuple((45,55,78,87))
+
+print(tpl1)
+print(tpl2)
+print(tpl1[1:])
+print(tpl1[1:3])
+print(type(tpl1))
+print(type(tpl2))
+
+tpl3 = (55,88,99,77,33,44,55)
+print(tpl3.index(88))
+
+lst = [88,75,69,22,45]
+print(type(lst))
+tpl = tuple(lst)
+print(tpl)
+print(type(tpl))

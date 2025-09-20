0dd82402ef9697a8c80ae8054f7d01fba08f1a1a
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-09-20 21:31:27 +0530

    while_loop

---
+++ while_loop.py
@@ -0,0 +1,19 @@
+i = 0
+while(i<6):
+    print('Hello')
+    i = i+1
+
+
+j = 1
+while(j<=10):
+    print(j)
+    j+=1
+
+sum = 0
+i = 1
+while(i<=5):
+    number = int(input("Enter Number : "))
+    sum = sum + number
+    i = i+1
+
+print("Sum of Numbers = " , sum)

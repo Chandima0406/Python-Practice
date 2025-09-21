af59fc27289799677a0539c447f3d84d5a8136b5
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-09-21 23:15:50 +0530

    default_argument

---
+++ default_argument.py
@@ -0,0 +1,6 @@
+def student(subject, marks, **friends):
+    print("Subject = ", subject)
+    print("markes = ", marks)
+    print("friends = ", friends)
+
+student("maths", 80, kamal = 80,nimal = 90)

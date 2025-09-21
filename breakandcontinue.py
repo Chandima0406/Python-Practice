166a732330898b472d2f771accd03c2d0c055a59
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-09-21 15:46:18 +0530

    break and continue

---
+++ breakandcontinue.py
@@ -0,0 +1,22 @@
+for x in range(1,6):
+    if(x==3):
+        continue
+    print(x)
+
+print("-----------------------")
+
+i = 0
+while(i<6):
+    i+=1
+    if (i == 3):
+        continue
+    print(i)
+
+print("-----------------------")
+
+for j in range(1,9):
+    if(j==5):
+        break
+    print(j)
+
+    

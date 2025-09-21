f28177bef4d4afa7d5c90c6f49373a5417165de3
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-09-21 14:58:37 +0530

    for_loop

---
+++ for_loop.py
@@ -0,0 +1,16 @@
+x = [1,2,3,4,5,6]
+
+for num in x:
+    print(num)
+
+y = {'name':'kamal','age':40, 'gender':'male'}
+
+for i,j in y.items():
+    print(i,j)
+
+
+for i in range(5):
+    print(i)
+
+for k in range(1,21):
+    print(k)

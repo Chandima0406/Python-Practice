11078105946d36ef2d76ef0d2ef2653e4cbf566c
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-09-21 23:05:10 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,21 @@
+def functionname(arg1):
+    print(arg1)
+
+functionname("Hello")
+
+print("-----------------")
+
+def sum(num1, num2):
+    print(num1 + num2)
+
+sum(55,90)
+
+print("-----------------")
+
+def func(length, width):
+    perimeter = 2*(length*width)
+    area = length*width
+    print(perimeter)
+    print(area)
+
+func(50,60)

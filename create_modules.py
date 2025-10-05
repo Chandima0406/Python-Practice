c9b733849cfc47e93f4d998b37f59278394e6801
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-10-05 21:41:47 +0530

    create_modules

---
+++ create_modules.py
@@ -0,0 +1,17 @@
+#addition
+def add(a,b):
+    return a + b
+    
+#substraction
+def sub(a,b):
+    return a - b
+
+#multiplication
+def mul(a,b):
+    return a*b
+
+#divition
+def div(a,b):
+    return a/b
+
+#me hadapu tika pawichi kranne main.py eke 
---
+++ main.py
@@ -0,0 +1,9 @@
+import create_modules
+
+i = 25
+j = 3
+
+print(create_modules.add(i,j))
+print(create_modules.mul(i,j))
+print(create_modules.sub(i,j))
+print(create_modules.div(i,j))

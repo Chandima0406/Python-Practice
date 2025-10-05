0b82c8242e62d6154dc4e3c0fac45b8b88d16957
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-10-05 21:01:45 +0530

    inheritance1

--- inheritance1.py
+++ inheritance1.py
@@ -4,8 +4,51 @@ class parent:
 
 class child(parent):
     def func2(self):
-        super().func1()
+        #super().func1()
         print("Welcome")
+
+    def func1(self):
+        print("Hi")
 
 myobject = child()
 myobject.func2()
+
+
+print("--------------------------")
+
+myobject.func1()
+
+
+print("--------------------------")
+
+class Fruit:
+    number_of_items = None
+    unit_price = None
+    def set_value(self,x,y):
+        self.number_of_items = x
+        self.unit_price = y
+
+class Apple(Fruit):
+    def price(self):
+        print('For Apple ', self.number_of_items*self.unit_price)
+
+class Orange(Fruit):
+    def price(self):
+        print('For Orange ', self.number_of_items*self.unit_price)
+
+class Grapes(Fruit):
+    def price(self):
+        print('For Grapes ', self.number_of_items*self.unit_price)
+
+
+object1 = Apple()
+object2 = Orange()
+object3 = Grapes()
+
+object1.set_value(50, 10)
+object2.set_value(6, 52)
+object3.set_value(20, 23)
+
+object1.price()
+object2.price()
+object3.price()
